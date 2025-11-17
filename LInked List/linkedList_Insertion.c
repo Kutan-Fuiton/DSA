e911441a82c3d7112779ce6a01369faa42501b3c
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2025-11-17 23:38:30 +0530

    commited

--- LInked List/linkedList_Insertion.c
+++ LInked List/linkedList_Insertion.c
@@ -17,14 +17,15 @@ void linkedLIstTraversal(struct Node *ptr){
 // A function which returns a node pointer which is basically a structure pointer --> takes argument as the head pointer and data to be inserted --> dynamically allocates memory for a new node, sets its next to the current head and sets its data to the given data --> then returns the new node pointer which will be the new head of the linked list
 struct Node * insertAtFirst(struct Node *head, int data){
     struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
-    ptr->next = head;
     ptr->data = data;
+
+    ptr->next = head;
     return ptr;
 }
 
 
-// A function which returns a node pointer which is basically a structure pointer --> takes argument as the head pointer and data to be inserted --> dynamically allocates memory for a new node, sets its next to the current head and sets its data to the given data --> then returns the new node pointer which will be the new head of the linked list
-struct Node * insertAtFirst(struct Node *head, int data, int index){
+// A function which returns a node pointer which is basically a structure pointer --> takes argument as the head pointer, data to be inserted and the index position where the node to be inserted --> dynamically allocates memory for a new node -->making the temporary node (structure node pointer) and assign the head, so that it can traverse with reference head --> then a while loop will execute from 0 to index - 2, which is to iterate the temporary node over the chain of nodes, i.e., linked list to the index i-1 --> after temp node getting to index - 1 position, extra node's data will be assign the data given by argument, and its next will point to temp node's next, and that temp node will point to extra node --> then returns the new node pointer which will be the new head of the linked list (will return the same thing in every case, that is head)
+struct Node * insertAtIndex(struct Node *head, int data, int index){
     struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
     struct Node *p = head;
     int i = 0;
@@ -32,6 +33,25 @@ struct Node * insertAtFirst(struct Node *head, int data, int index){
         p = p->next;
         i++;
     }
+    ptr->data = data;
+    ptr->next = p->next;
+    p->next = ptr;
+    return head;
+}
+
+
+
+struct Node * insertAtEnd(struct Node *head, int data){
+    struct Node *ptr = (struct Node*)malloc(sizeof(struct Node));
+    ptr->data = data;
+    struct Node *p = head;
+
+    while (p->next != NULL){
+        p = p->next;
+    }
+    p-> next = ptr;
+    ptr->next = NULL;
+    return head;
 }
 
 int main() {
@@ -62,6 +82,12 @@ int main() {
     fourth->data = 66;
     fourth->next = NULL;
 
+    printf("Linked list before insertion\n");
+    linkedLIstTraversal(head);
+    // head = insertAtFirst(head, 56);
+    // head = insertAtIndex(head, 56, 1);
+    head = insertAtEnd(head, 56);
+    printf("Linked list after insertion\n");
     linkedLIstTraversal(head);
 
     
