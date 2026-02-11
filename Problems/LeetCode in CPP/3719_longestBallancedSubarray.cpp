814e974e19f733c7254f6c41b098bb96d6128b08
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2026-02-11 10:33:45 +0530

    leetcode

---
+++ Problems/LeetCode in CPP/3719_longestBallancedSubarray.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            unordered_set<int> even, odd;
+
+            for (int j = i; j < n; j++) {
+                if (nums[j] % 2 == 0) even.insert(nums[j]);
+                else odd.insert(nums[j]);
+
+                if (even.size() == odd.size()) {
+                    ans = max(ans, j - i + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+int main(){
+    Solution sol;
+    vector<int> nums = {3,2,2,5,4};
+
+    cout << sol.longestBalanced(nums) << endl;
+
+    return 0;
+}
