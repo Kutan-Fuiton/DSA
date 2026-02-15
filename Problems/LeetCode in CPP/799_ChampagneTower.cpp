48554c0832aec07091893f5b81ec1b421bce47aa
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2026-02-15 10:59:31 +0530

    LeetCode

---
+++ Problems/LeetCode in CPP/799_ChampagneTower.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> dp(query_row + 2,vector<double>(query_row + 2, 0.0));
+        dp[0][0] = poured;
+        for (int i = 0; i <= query_row; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (dp[i][j] > 1.0) {
+                    double overflow = dp[i][j] - 1.0;
+                    dp[i][j] = 1.0;
+                    dp[i+1][j] += overflow / 2.0;
+                    dp[i+1][j+1] += overflow / 2.0;
+                }
+            }
+        }
+        return min(1.0, dp[query_row][query_glass]);
+    }
+};
+
+
+int main(){
+    Solution sol;
+
+    // cout << sol.champagneTower(4, 2, 0) << endl;
+    cout << sol.champagneTower(5, 3, 2) << endl;
+
+    return 0;
+}
