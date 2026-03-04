a38c1ec388fb6384c769ff7a6c3da3716b8fabb9
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2026-03-04 21:08:38 +0530

    Daily LeetCode code push

---
+++ Problems/LeetCode in CPP/3666_minOpsToEqualiseBinString.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int minOperations(string s, int k) {
+        
+    }
+};
+
+int main(){
+    Solution sol;
+    cout << sol.minOperations("0101", 3) << endl;
+    return 0;
+}
