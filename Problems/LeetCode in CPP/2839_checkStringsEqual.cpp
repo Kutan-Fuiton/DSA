d0774104f944d307b977e7691cf5517ac1946550
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2026-03-30 07:29:45 +0530

    Daily LeetCode code push

---
+++ Problems/LeetCode in CPP/2839_checkStringsEqual.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        if (s1 == s2) return true;
+
+        if (s1[0] == s2[2] && s1[2] == s2[0] && s1[1] == s2[1] && s1[3] == s2[3]) return true;
+        if (s1[1] == s2[3] && s1[3] == s2[1] && s1[0] == s2[0] && s1[2] == s2[2]) return true;
+        if (s1[1] == s2[3] && s1[3] == s2[1] && s1[0] == s2[2] && s1[2] == s2[0]) return true;
+
+        return false;
+    }
+};
+
+int main(){
+    Solution sol;
+    cout << sol.canBeEqual("cdab", "abcd") << endl;
+    return 0;
+}
