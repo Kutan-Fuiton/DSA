462294dca1466a81cecc1e59ede0efe90db1f575
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2026-07-16 01:32:03 +0530

    Daily LeetCode code push

---
+++ Problems/LeetCode in CPP/3658_GCDofOddEvenSums.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+};
+
+int main(){
+    Solution sol;
+    cout << sol.gcdOfOddEvenSums(5) << endl;
+    return 0;
+}
