ed0b01abd325f7fb1c1c4bdb8627db257b7e7144
Subarno Chakraborty <subarnochakraborty123@gmail.com>
2026-08-26 11:45:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (27.96%) - LeetSync

---
+++ leetcode-problem-solving/3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    string shortestBeautifulSubstring(string s, int k) {
+        int n = s.size();
+        queue<int> q;
+        string best = "";
+        int min_len = INT_MAX;
+
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == '1') {
+                q.push(i);
+            }
+
+            if (q.size() > k) {
+                q.pop();
+            }
+
+            if (q.size() == k) {
+                int start = q.front();
+                int end = q.back();
+                int len = end - start + 1;
+                string sub = s.substr(start, len);
+
+                if (len < min_len) {
+                    min_len = len;
+                    best = sub;
+                } else if (len == min_len) {
+                    best = min(best, sub); // Lexicographically smallest
+                }
+            }
+        }
+
+        return best;
+    }
+};
